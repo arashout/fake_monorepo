47035e4040517c188bd3a6445d376acbe60dad66
arash outadi <arash.out@gmail.com>
2023-01-25 16:04:26 -0800

    Adding another package

---
+++ go/src/fakeorg/artifactory/gopackagetest/go.mod
@@ -0,0 +1,3 @@
+module github.com/arashout/fake_monorepo/go/src/fakeorg/artifactory/gopackagetest
+
+go 1.19
--- go/src/fakeorg/artifactory/gopackagetest/main.go
+++ go/src/fakeorg/artifactory/gopackagetest/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/arashout/fake_monorepo/go/src/fakeorg/artifactory/anothertestpackage/beef"
 )
 
 func main() {
+	fmt.Println(beef.SirloinSteak())
 	fmt.Println("This is a gopackagetest")
 }
